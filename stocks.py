5db424b0dae411e72468b6b14d72ac85a715f8a0
Jing Yang <bh_jingyang@hotmail.com>
2019-07-08 02:20:53 +0800

    ok

--- stocks.py
+++ stocks.py
@@ -1,2 +1,6 @@
-import tushare
-print(tushare.__version__)
+import numpy as np
+import pandas as pd
+import tushare as ts
+
+data = ts.get_hist_data('sh',start='2017-01-01',end='2019-07-07')
+money = ts.get_money_supply()
